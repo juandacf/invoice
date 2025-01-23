9f620efc9159ea39331b5e2ed891bcbafc3487fa
Juan Caballero <juandacf@gmail.com>
2025-01-23 12:03:37 -0500

    feat add: delete button & subtotal update in final card

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const mainContainer = {}
 function addProduct(products){
         const myTable = document.getElementById("productTable");
         const subTotal = products.quantity * products.unitValue;
-
+        const elementClass = new Date().getMilliseconds().toString(16)
         const newTableElement = document.createElement('tr');
+
+        newTableElement.classList.add(elementClass)
         newTableElement.innerHTML = /*html*/`
 
             <td> ${products.productCode}</td>
@@ -12,9 +14,19 @@ function addProduct(products){
             <td> ${products.unitValue}</td>
             <td> ${products.quantity}</td>
             <td> ${subTotal}</td>
-            <td class="bold deleteButton" id="${products.productCode}"> Delete</td>
+            <td class="bold deleteButton" id="${elementClass}"> X</td>
         `
         myTable.append(newTableElement);
+
+        const deleteButton =  document.getElementById(elementClass)
+        deleteButton.addEventListener("click", ()=>{
+            newTableElement.remove()
+            console.log(`El elemento con la clase ${elementClass} ha sido eliminado`)
+        })
+
+        var subTotalCard = parseInt(document.getElementById("subTotal").innerHTML);
+        document.getElementById("subTotal").innerHTML = subTotalCard + subTotal;
+        
     }
 
 
@@ -25,3 +37,7 @@ myForm.addEventListener("submit", async(e)=>{
     let data= Object.fromEntries(new FormData(e.target));
     addProduct(data);
 })
+
+
+
+
