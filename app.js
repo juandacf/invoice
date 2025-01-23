1981634392b2108a93cef78231b2fea0f15b2889
Juan Caballero <juandacf@gmail.com>
2025-01-23 14:07:24 -0500

    add iva calculator/total amount

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ function addProduct(products){
         const subTotal = products.quantity * products.unitValue;
         const elementClass = new Date().getMilliseconds().toString(16)
         const newTableElement = document.createElement('tr');
+        const Iva = document.getElementById("iva");
+        const finalAmount = document.getElementById("finalAmount");
 
         newTableElement.classList.add(elementClass)
         newTableElement.innerHTML = /*html*/`
@@ -13,20 +15,27 @@ function addProduct(products){
             <td> ${products.productName}</td>
             <td> ${products.unitValue}</td>
             <td> ${products.quantity}</td>
-            <td> ${subTotal}</td>
+            <td class="subTotal" id="${elementClass}-subTotal"> ${subTotal}</td>
             <td class="bold deleteButton" id="${elementClass}"> X</td>
         `
         myTable.append(newTableElement);
 
+
+
+        var subTotalCard = document.getElementById("subTotal");
+        subTotalCard.innerHTML = parseInt(subTotalCard.innerHTML) + parseInt(subTotal);
+        Iva.innerHTML = parseInt(subTotalCard.innerHTML) * 0.19;
+        finalAmount.innerHTML= parseInt(subTotalCard.innerHTML)+ parseFloat(Iva.innerHTML);
+        
         const deleteButton =  document.getElementById(elementClass)
         deleteButton.addEventListener("click", ()=>{
-            newTableElement.remove()
-            console.log(`El elemento con la clase ${elementClass} ha sido eliminado`)
+            subTotalCard.innerHTML = parseInt(subTotalCard.innerHTML) - parseInt(document.getElementById(`${elementClass}-subTotal`).innerHTML)
+            Iva.innerHTML = parseInt(subTotalCard.innerHTML) * 0.19;
+            finalAmount.innerHTML= parseInt(subTotalCard.innerHTML)+ parseFloat(Iva.innerHTML);
+            newTableElement.remove();
         })
 
-        var subTotalCard = parseInt(document.getElementById("subTotal").innerHTML);
-        document.getElementById("subTotal").innerHTML = subTotalCard + subTotal;
-        
+    
     }
 
 
